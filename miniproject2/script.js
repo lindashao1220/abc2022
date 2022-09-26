5f1d08195558c2db2758fe2df0c323d6a3b8927c
lindashao1220 <ys4950@nyu.edu>
2022-09-26 21:31:40 +0800

    changes

--- miniproject2/script.js
+++ miniproject2/script.js
@@ -4,7 +4,7 @@ let sh = screen.height;
 let popWidth = 400;
 let popHeight = 400;
 
-let windowNames = ["popup2", "popup3", "popup4", "popup5"];
+let windowNames = ["popup2/index.html", "popup3/index.html", "popup4/index.html", "popup5/index.html"];
 let yPos = [20,220,340,700];
 let openWindows = [];
 
@@ -38,6 +38,7 @@ function greenLight(){
             if(x > sw-200){
               clearInterval(interval);
               win.close();
+              console.log("1");
               redLight();
             }else{
               win.moveTo(x,yPos[i]);
@@ -47,24 +48,15 @@ function greenLight(){
       }
 
 function redLight(){
-  for(let i =0; i<5; i++){
-    let stp = window.open(popup6, "", "width="+sw+", height="+sh+", left="+sw*0.4+", top="+sh*0.3+"");
-    setTimeout(function(){
-      stp.close();
-      greenLight();
-      // openManyWindows(4);
-  }, 1500)
-    console.log(index);
-  // stp.addEventListener("load", ()=>{
-  //   setTimeout(function(){
-  //     stp.close();
-  //   }, 1000);
-  // })
-  // }
+  for(let i =0; i<4; i++){
+    let red = window.open("popup6/index.html", "", "width="+sw+", height="+sh+", left="+sw*0.4+", top="+sh*0.3+"");
+    red.addEventListener("load", ()=>{
+      setTimeout(function(){
+        red.close();
+      }, 1000);
+    })
+  }
 }
-}
-
-
         // in each loop:
             // we get the windowName out of windowNames using i
             // use the name to open a window like this:
@@ -75,50 +67,3 @@ function redLight(){
             // generate random number for speed
             // start an interval to move win across using the random speed
             // inside interval have also a closing mechanism (ie when window reach max width, close it something)
-
-
-
-
-
-
-
-        //let win1 = window.open("popup2", "", "width=250, height=130, left="+x+", top="+ranY+"");
-        
-        // for(let index=2; index<4; index++){
-        //   index++;
-        //  }
-
-        // let interval[index] = setInterval(()=>{
-        //   x+=5;
-        //   if(x > sw-300){
-        //     clearInterval(interval[index]);
-        //     win[index].close();
-        //   }else{
-        //     win[index].moveTo(x, ranY+200);
-        //   }
-        // }, 50)
-
-        // let interval = setInterval(()=>{
-        //     x+=5;
-        //     if(x > sw-300){
-        //       clearInterval(interval);
-        //       win2.close();
-        //     }else{
-        //       win2.moveTo(x, ranY+200);
-        //     }
-        //   }, 50)
-
-        //   let interval1 = setInterval(()=>{
-        //     x+=5;
-        //     if(x > sw-300){
-        //       clearInterval(interval1);
-        //       win3.close();
-        //     }else{
-        //       win3.moveTo(x, ranY+400);
-        //     }
-        //   }, 50)
-
-
-
-
-
