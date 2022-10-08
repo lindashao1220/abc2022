0f3c817dbf6aaaabb8cfa9c67211ae2e241c1756
lindashao1220 <ys4950@nyu.edu>
2022-10-08 23:01:00 +0800

    changes

--- projectA/script.js
+++ projectA/script.js
@@ -3,21 +3,25 @@ let button = document.getElementById("button");
 let sw = screen.width;
 let sh = screen.height;
 
+let ranX = Math.random(0.4,0.6)*sh;
+let y = sh;
+
+
+let windowNames = ["popup0/index.html", "popup1/index.html", "popup2/index.html", "popup3/index.html"];
+let xPos = [20,220,340,700,800,100];
+let yPos = [20,220,340,400,500,100];
+let openWindows = [];
+
 function topStar(){
-  let ranX = Math.random()*sh - 200;
-  let y = sh;
   let win = window.open("star", "_blank", "width=30, height=30, left="+y+", top="+ranX);
 
   win.addEventListener("load", ()=>{
     let interval = setInterval(()=>{
-
-      // increasing the value of x in every interval
       y-=20;
-
-      if(y < 10){
+      if(y < 60){
         clearInterval(interval);
-        // and close the star window
         win.close();
+        openMoreWindow();
       }else{
         win.moveTo(ranX, y);
       }
@@ -32,14 +36,20 @@ function shootingStars(){
   inter = setInterval(topStar(),200);
 }
 
-
-// function stop(){
-//   // clear the interval and show star button / hide stop button
-//   clearInterval(inter);
-//   button.style.display = "block";
-//   // enough.style.display = "none";
-// }
-
-// add listeners, done!
 button.addEventListener("click", shootingStars);
-// enough.addEventListener("click", stop);
+
+
+function openMoreWindow(){
+  for(let i=0; i<windowNames.length; i++){
+    let url = windowNames[i];
+    let win = window.open(url, "", "width=150, height=200, left="+ranX+", top="+(i*200));
+    setTimeout(function(){
+      win.close();
+  }, 1500)
+  }
+}
+
+if(y < 60){
+  openMoreWindow();
+
+}
