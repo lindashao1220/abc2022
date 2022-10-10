c1177811c61d9f5671eac55ed445784dbd87f1b5
lindashao1220 <ys4950@nyu.edu>
2022-10-10 10:37:29 +0800

    changes

--- projectA/script.js
+++ projectA/script.js
@@ -3,14 +3,16 @@ let button = document.getElementById("button");
 let sw = screen.width;
 let sh = screen.height;
 
-let ranX = Math.random(0.4,0.6)*sh;
+let ranX = Math.random(0.7,)*sh;
+let xPos1 = ranX +80;
+let xPos2 = ranX -50;
 let y = sh;
 
+//let windowNames = ["popup0/index.html", "popup1/index.html", "popup2/index.html", "popup3/index.html"];
+// let xPos = [20,220,340,700,800,100];
+// let yPos = [20,220,340,400,500,100];
 
-let windowNames = ["popup0/index.html", "popup1/index.html", "popup2/index.html", "popup3/index.html"];
-let xPos = [20,220,340,700,800,100];
-let yPos = [20,220,340,400,500,100];
-let openWindows = [];
+alert("Hello! Firework is coming:)");
 
 function topStar(){
   let win = window.open("star", "_blank", "width=30, height=30, left="+y+", top="+ranX);
@@ -29,9 +31,9 @@ function topStar(){
   })
 }
 
-let inter;
 
 function shootingStars(){
+  let inter;
   button.style.display = "none";
   inter = setInterval(topStar(),200);
 }
@@ -40,16 +42,42 @@ button.addEventListener("click", shootingStars);
 
 
 function openMoreWindow(){
-  for(let i=0; i<windowNames.length; i++){
-    let url = windowNames[i];
-    let win = window.open(url, "", "width=150, height=200, left="+ranX+", top="+(i*200));
+    let win0 = window.open("popup0/index.html", "", "width=25, height=25, left="+xPos1+", top="+2);
+    let win1 = window.open("popup1/index.html", "", "width=25, height=25, left="+xPos1+", top="+190);
+    let win2 = window.open("popup2/index.html", "", "width=25, height=25, left="+xPos2+", top="+2);
+    let win3 = window.open("popup3/index.html", "", "width=25, height=25, left="+xPos2+", top="+190);
+
+       //slower down the for loop
+        const sleep = (time) => {
+          return new Promise((resolve) => setTimeout(resolve, time))
+        }
+
+        const biggerWindow = async () => {
+          for(let i=0; i<8; i++){
+          await sleep(500);
+          console.log(10+i*5);
+
+            // ???  for(let i=0; i<windowNames.length; i++){
+            //  console.log("win"+[i]);
+            //  let windows = "win"+[i];
+            //}
+        win0.resizeBy(3+i*1, 3+1*i);
+        win1.resizeBy(3+i*1, 3+1*i);
+        win2.resizeBy(3+i*1, 3+1*i);
+        win3.resizeBy(3+i*1, 3+1*i);
+        }
+      }
+      setTimeout(biggerWindow(), 4000)
+
     setTimeout(function(){
-      win.close();
-  }, 1500)
+      win0.close();
+      win1.close();
+      win2.close();
+      win3.close();
+  }, 4000)
   }
-}
+
 
 if(y < 60){
   openMoreWindow();
-
 }
