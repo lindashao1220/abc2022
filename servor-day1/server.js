f94679496d364c5728bbad8cc8f6589b230679de
lindashao1220 <ys4950@nyu.edu>
2022-11-08 15:51:10 +0800

    changes

---
+++ servor-day1/server.js
@@ -0,0 +1,24 @@
+console.log("hello i am linda on the server");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World! nihao')
+})
+
+app.get('/swimming', (req, res) => {
+    res.send('~~~~~~~~~~')
+  })
+
+// app.get('/treehouse', (req, res) => {
+//     console.log(req);
+//     res.sendFile(__dirname+"/treehouse/index.html");
+//   })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
