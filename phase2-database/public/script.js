8efeb01701b4dcf3cde2cc5bd7258aa67e5ab007
lindashao1220 <ys4950@nyu.edu>
2023-03-06 22:52:35 +0800

    changes

---
+++ phase2-database/public/script.js
@@ -0,0 +1,668 @@
+var socket = io();
+console.log("hiiii:)))");
+
+
+// NEW1: ml5
+let video;
+let faceapi;
+let detections;
+const detection_options = {
+  withLandmarks: true,
+  withDescriptors: false,
+}
+
+
+let cnv;
+var webCam;
+var webCamImage;
+var snap;
+var slider;
+let zoom = 4;
+// let show = true;
+
+const texts =[];
+
+function setup() {
+  let canvas = createCanvas(360, 270);
+  canvas.parent("container");
+  canvas.id("cnv");
+  noCanvas();
+
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide(); 
+  // Hide the video element, and just show the canvas
+  // faceapi = ml5.faceApi(video, detection_options, modelMouthReady)
+  // textAlign(RIGHT);
+}
+
+
+function modelMouthReady() {
+  console.log('ready!');
+  console.log(faceapi);
+  faceapi.detect(gotMouthResults);
+}
+
+
+function modelNoseReady() {
+  console.log('ready!')
+  console.log(faceapi)
+  faceapi.detect(gotNoseResults)
+}
+
+function modelLefteyeReady() {
+  console.log('ready!')
+  console.log(faceapi)
+  faceapi.detect(gotLefteyeResults)
+}
+
+function modelRighteyeReady() {
+  console.log('ready!')
+  // console.log(faceapi)
+  faceapi.detect(gotRighteyeResults)
+}
+
+function modelForeheadeyeReady() {
+  console.log('ready!')
+  console.log(faceapi)
+  faceapi.detect(gotForeheadResults)
+}
+
+
+
+function gotMouthResults(err, result) {
+  if (err) {
+      console.log(err)
+      return
+  }
+  // console.log(result)
+  detections = result;
+
+  // background(220);
+  background(255);
+  image(video, 0,0, width, height)
+  if (detections) {
+      if (detections.length > 0) {
+          // console.log(detections)
+          drawBox(detections)
+          drawMouthLandmarks(detections)
+      }
+  }
+  faceapi.detect(gotMouthResults)
+}
+
+
+function gotNoseResults(err, result) {
+  if (err) {
+      console.log(err)
+      return
+  }
+  // console.log(result)
+  detections = result;
+
+  // background(220);
+  background(255);
+  image(video, 0,0, width, height)
+  if (detections) {
+      if (detections.length > 0) {
+          // console.log(detections)
+          drawBox(detections)
+          drawNoseLandmarks(detections)
+      }
+
+  }
+  faceapi.detect(gotNoseResults)
+}
+
+
+function gotLefteyeResults(err, result) {
+  if (err) {
+      console.log(err)
+      return
+  }
+  // console.log(result)
+  detections = result;
+
+  // background(220);
+  background(255);
+  image(video, 0,0, width, height)
+  if (detections) {
+      if (detections.length > 0) {
+          // console.log(detections)
+          drawBox(detections)
+          drawLefteyeLandmarks(detections)
+      }
+
+  }
+  faceapi.detect(gotLefteyeResults)
+}
+
+function gotRighteyeResults(err, result) {
+  if (err) {
+      console.log(err)
+      return
+  }
+  // console.log(result)
+  detections = result;
+
+  // background(220);
+  background(255);
+  image(video, 0,0, width, height)
+  if (detections) {
+      if (detections.length > 0) {
+          // console.log(detections)
+          drawBox(detections)
+          drawRighteyeLandmarks(detections)
+      }
+
+  }
+  faceapi.detect(gotRighteyeResults)
+}
+
+
+function gotForeheadResults(err, result) {
+  if (err) {
+      console.log(err)
+      return
+  }
+  // console.log(result)
+  detections = result;
+
+  // background(220);
+  background(255);
+  image(video, 0,0, width, height)
+  if (detections) {
+      if (detections.length > 0) {
+          // console.log(detections)
+          drawBox(detections)
+          drawForeheadLandmarks(detections)
+      }
+  }
+  faceapi.detect(gotForeheadResults)
+}
+
+
+
+
+function drawBox(detections){
+  for(let i = 0; i < detections.length; i++){
+      const alignedRect = detections[i].alignedRect;
+      const x = alignedRect._box._x
+      const y = alignedRect._box._y
+      const boxWidth = alignedRect._box._width
+      const boxHeight  = alignedRect._box._height
+
+      noFill();
+      stroke(161, 95, 251);
+      strokeWeight(2);
+      rect(x, y, boxWidth, boxHeight);
+  }
+  
+}
+
+
+
+let imgData;
+
+function drawMouthLandmarks(detections){
+  noFill();
+  stroke(161, 95, 251)
+  strokeWeight(2)
+
+
+let minX = 9999;
+let minY = 9999;
+let maxX = 0;
+let maxY = 0;
+
+  for(let i = 0; i < detections.length; i++){
+      const mouth = detections[i].parts.mouth; 
+      for(let i = 0; i < mouth.length; i++){
+        if (mouth[i]._x > maxX) {
+          maxX = mouth[i]._x;
+        }
+        if (mouth[i]._y > maxY) {
+          maxY = mouth[i]._y;
+        }
+        if (mouth[i]._x < minX) {
+          minX = mouth[i]._x;
+        }
+        if (mouth[i]._y < minY) {
+          minY = mouth[i]._y;
+        }
+      }
+
+      imgData = cropToDataUrl(video, minX-60, minY-2, maxX-minX+115, maxY-minY+40, 50, 50, maxX-minX+115, maxY-minY+40);
+      copy(minX-60, minY-2, maxX-minX+115, maxY-minY+40, 50, 50, maxX-minX+115, maxY-minY+40);
+
+      // copy(minX, minY, maxX-minX, maxY-minY, 0, 0, maxX-minX, maxY-minY);
+
+  }
+}
+
+
+function drawNoseLandmarks(detections){
+  noFill();
+  stroke(161, 95, 251);
+  strokeWeight(2);
+
+  for(let i = 0; i < detections.length; i++){
+      const nose = detections[i].parts.nose;
+
+      let minX = 9999;
+      let minY = 9999;
+      let maxX = 0;
+      let maxY = 0;
+      for(let i = 0; i < nose.length; i++){
+        if (nose[i]._x > maxX) {
+          maxX = nose[i]._x;
+        }
+        if (nose[i]._y > maxY) {
+          maxY = nose[i]._y;
+        }
+        if (nose[i]._x < minX) {
+          minX = nose[i]._x;
+        }
+        if (nose[i]._y < minY) {
+          minY = nose[i]._y;
+        }
+      }
+
+      fill(0, 255, 0);
+
+      imgData = cropToDataUrl(video, minX-73, minY+20, maxX-minX+150, maxY-minY-10, 50, 50, maxX-minX+150, maxY-minY-10);
+      copy(minX-73, minY+20, maxX-minX+150, maxY-minY-10, 50, 50, maxX-minX+150, maxY-minY-10);
+  }
+}
+
+
+function drawLefteyeLandmarks(detections){
+  noFill();
+  stroke(161, 95, 251);
+  strokeWeight(2);
+
+  for(let i = 0; i < detections.length; i++){
+      const leftEye = detections[i].parts.rightEye;
+
+      let minX = 9999;
+      let minY = 9999;
+      let maxX = 0;
+      let maxY = 0;
+      for(let i = 0; i < leftEye.length; i++){
+        if (leftEye[i]._x > maxX) {
+          maxX = leftEye[i]._x;
+        }
+        if (leftEye[i]._y > maxY) {
+          maxY = leftEye[i]._y;
+        }
+        if (leftEye[i]._x < minX) {
+          minX = leftEye[i]._x;
+        }
+        if (leftEye[i]._y < minY) {
+          minY = leftEye[i]._y;
+        }
+      }
+
+      fill(0, 255, 0);
+
+
+      imgData = cropToDataUrl(video, minX-20, minY-10, maxX-minX+65, maxY-minY+30, 50, 50, maxX-minX+65, maxY-minY+30);
+
+      copy(minX-20, minY-10, maxX-minX+65, maxY-minY+30, 50, 50, maxX-minX+65, maxY-minY+30);
+  }
+}
+
+function drawRighteyeLandmarks(detections){
+  noFill();
+  stroke(161, 95, 251);
+  strokeWeight(2);
+
+  for(let i = 0; i < detections.length; i++){
+      const rightEye = detections[i].parts.leftEye;
+
+      // const rightEyeBrow = detections[i].parts.rightEyeBrow;
+      // const leftEyeBrow = detections[i].parts.leftEyeBrow;
+
+      let minX = 9999;
+      let minY = 9999;
+      let maxX = 0;
+      let maxY = 0;
+      for(let i = 0; i < rightEye.length; i++){
+        if (rightEye[i]._x > maxX) {
+          maxX = rightEye[i]._x;
+        }
+        if (rightEye[i]._y > maxY) {
+          maxY = rightEye[i]._y;
+        }
+        if (rightEye[i]._x < minX) {
+          minX = rightEye[i]._x;
+        }
+        if (rightEye[i]._y < minY) {
+          minY = rightEye[i]._y;
+        }
+      }
+
+      fill(0, 255, 0);
+
+imgData = cropToDataUrl(video, minX-43, minY-10, maxX-minX+65, maxY-minY+30, 50, 50, maxX-minX+60, maxY-minY+30);
+copy(minX-43, minY-10, maxX-minX+65, maxY-minY+30, 50, 50, maxX-minX+60, maxY-minY+30);
+
+  }
+}
+
+
+function drawForeheadLandmarks(detections){
+  noFill();
+  stroke(161, 95, 251);
+  strokeWeight(2);
+
+  for(let i = 0; i < detections.length; i++){
+      const foreheadRight = detections[i].parts.rightEyeBrow;
+      const foreheadLeft = detections[i].parts.leftEyeBrow;
+    
+
+      let minX = 9999;
+      let minY = 9999;
+      let maxX = 0;
+      let maxY = 0;
+      for(let i = 0; i < foreheadRight.length; i++){
+        if (foreheadRight[i]._x > maxX) {
+          maxX = foreheadRight[i]._x;
+        }
+        if (foreheadRight[i]._y > maxY) {
+          maxY = foreheadRight[i]._y;
+        }
+        if (foreheadRight[i]._y < minY) {
+          minY = foreheadRight[i]._y;
+        }
+      }
+
+      for(let i = 0; i < foreheadLeft.length; i++){
+      if (foreheadLeft[i]._x < minX) {
+        minX = foreheadLeft[i]._x;
+      }
+    }
+
+      fill(0, 255, 0);
+
+imgData = cropToDataUrl(video, minX-25, minY-60, maxX-minX+50, maxY-minY+65, 50, 50, maxX-minX+50, maxY-minY+65);
+copy(minX-25, minY-60, maxX-minX+50, maxY-minY+65, 50, 50, maxX-minX+50, maxY-minY+65);
+
+  }
+}
+
+
+function drawPart(feature, closed){
+  beginShape();
+  for(let i = 0; i < feature.length; i++){
+      const x = feature[i]._x
+      const y = feature[i]._y
+      vertex(x, y)
+  }
+  if(closed === true){
+      endShape(CLOSE);
+  } else {
+      endShape();
+  }
+  
+}
+
+
+let cases;
+socket.on("lefteye", data=>{
+  console.log(data);
+  if (data == "lefteye"){
+    faceapi = ml5.faceApi(video, detection_options, modelLefteyeReady)
+    textAlign(RIGHT);
+    cases = 1;
+    console.log("ok here goes me!!!!!")
+  }
+})
+
+socket.on("righteye", data=>{
+  console.log(data);
+  if (data == "righteye"){
+    faceapi = ml5.faceApi(video, detection_options, modelRighteyeReady)
+    textAlign(RIGHT);
+    cases = 2;
+  }
+})
+
+socket.on("nose", data=>{
+  console.log(data);
+  if (data == "nose"){
+    faceapi = ml5.faceApi(video, detection_options, modelNoseReady)
+    textAlign(RIGHT);
+    cases = 3;
+  }
+})
+
+socket.on("forehead", data=>{
+  console.log(data);
+  if (data == "forehead"){
+    faceapi = ml5.faceApi(video, detection_options, modelForeheadeyeReady)
+    textAlign(RIGHT);
+    cases = 4;
+  }
+})
+
+socket.on("mouth", data=>{
+  console.log(data);
+  if (data == "mouth"){
+    faceapi = ml5.faceApi(video, detection_options, modelMouthReady)
+    textAlign(RIGHT);
+    cases = 5;
+  }
+})
+
+
+
+
+function takePhoto(){
+  console.log("hiiii");
+  console.log(imgData);
+  
+  // document.getElementById('test').style.background = 'url(' + imgData + ')';
+  document.getElementById("output").src = imgData;
+}
+
+
+
+let store;
+let btn = document.getElementById("button");
+btn.addEventListener("click", ()=>{
+  takePhoto();
+  store = imgData;
+
+  if (confirm("Can I collect your face data?")) {
+    takePhoto();
+    store = imgData;
+    if(imgData === undefined){
+      console.log("hiahsiskbc轻松点");
+      alert("Didn't recognise your face😞 Could you contribute again?")
+    }else{
+      alert("Thanks for your contribution🤪");
+    }
+  } else {
+    alert("I understand your concern🙂 Plz continue enjoying the face collage🥰");
+  }
+})
+
+
+
+//send it to database
+//not to the collage
+// //send message to the server
+let sendbutton = document.getElementById("send");
+  sendbutton.addEventListener("click", ()=>{
+    console.log("clicked");
+        //send name message to the server(package up the data)
+        let data ={imgChange: store}
+        //send the message to the server
+        if(cases == 1){
+          console.log("imgLefteyeChange");
+          socket.emit('imgLefteyeChange', data);
+        }
+        else if(cases == 2){
+          console.log("imgRighteyeChange");
+          socket.emit('imgRighteyeChange', data);
+        }
+        else if(cases == 3){
+          console.log("imgNoseChange");
+          socket.emit('imgNoseChange', data);
+
+        } else if(cases == 4){
+          console.log("imgForeheadChange");
+          socket.emit('imgForeheadChange', data);
+
+        } else if(cases == 5){
+          console.log("imgMouthChange");
+          socket.emit('imgMouthChange', data);
+        }
+        console.log(data);
+
+        alert("Submitted successfully🤩 Hope you enjoy the collage🥳")
+
+})
+
+
+function cropToDataUrl(image, x, y, w, h) {
+  let graphics = createGraphics(w, h);
+  graphics.copy(image, x, y, x + w, y + h, 0, 0, x + w, y + h);
+  let dataUrl = graphics.canvas.toDataURL();
+  return dataUrl;
+}
+
+
+
+
+
+
+socket.on("archivalLefteyeData", data=>{
+  console.log("archivalLefteyeData")
+  document.getElementById("recieve1").src = data;
+})
+
+socket.on("archivalRighteyeData", data=>{
+  console.log("archivalRighteyeData")
+  document.getElementById("recieve2").src = data;
+})
+
+socket.on("archivalNoseData", data=>{
+  console.log("archivalNoseData")
+  document.getElementById("recieve5").src = data;
+})
+
+socket.on("archivalForeheadData", data=>{
+  console.log("archivalForeheadData")
+  document.getElementById("recieve4").src = data;
+})
+
+socket.on("archivalMouthData", data=>{
+  console.log("archivalMouthData")
+  document.getElementById("recieve3").src = data;
+})
+
+
+
+
+
+
+
+
+
+
+// let myId;
+// document.getElementById("myId").innerText = socket.id;
+
+// socket.on("socketInfo", data=>{
+//     myId = data.id;
+
+  texts.push("PHOTO",
+  "COLLAGE",
+  "照片",
+  "拼贴",
+  "Wanna Join?",
+  ":)")   
+
+// })
+
+
+
+
+
+/*reference: https://alvarotrigo.com/blog/css-text-animations/ */
+const elts = {
+  text1: document.getElementById("text1"),
+  text2: document.getElementById("text2")
+};
+
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+  morph -= cooldown;
+  cooldown = 0;
+
+  let fraction = morph / morphTime;
+
+  if (fraction > 1) {
+      cooldown = cooldownTime;
+      fraction = 1;
+  }
+
+  setMorph(fraction);
+}
+
+function setMorph(fraction) {
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+  morph = 0;
+
+  elts.text2.style.filter = "";
+  elts.text2.style.opacity = "100%";
+
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 1000;
+  time = newTime;
+
+  cooldown -= dt;
+
+  if (cooldown <= 0) {
+      if (shouldIncrementIndex) {
+          textIndex++;
+      }
+
+      doMorph();
+  } else {
+      doCooldown();
+  }
+}
+
+animate();
---
+++ phase2-database/server.js
@@ -0,0 +1,361 @@
+const util = require('util')
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("../phase2-database/node_modules/socket.io/dist");
+const io = new Server(server);
+
+
+app.use(express.static('public'));
+
+
+
+
+//DATABASE
+let admin = require("firebase-admin");
+
+// Fetch the service account key JSON file contents
+let serviceAccount = require("./key/databaseforopenproject-firebase-adminsdk-qe1z9-b4cf6bbae8.json");
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  // The database URL depends on the location of the database
+  databaseURL: "https://databaseforopenproject-default-rtdb.firebaseio.com"
+});
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref0 = db.ref("lefteye");
+var ref1 = db.ref("righteye");
+var ref2 = db.ref("nose");
+var ref3 = db.ref("forehead");
+var ref4 = db.ref("mouth");
+// ref.push("byee")
+
+
+io.on('connection', (socket) => {
+//   all we want to do with the client, we need to do in here.
+    console.log('------------------------------------');
+    console.log('[+] a user connected', socket.id);
+    let clientObject = {
+        id: socket.id,
+
+    }
+
+    
+let counter = 0;
+
+let randomLefteyeItem;
+let countLefteyeCur; 
+let arrLefteye = [];
+
+//get the current numbr of the file in database to allocate part
+ref0.once('value').then((snapshot) => {
+  countLefteyeCur = snapshot.numChildren();
+  console.log("heyyyy i am always here", countLefteyeCur)
+  counter = counter + 1;
+})
+
+//tuen all the data in database into an image array
+  ref0.once('value').then((snapshot) => {
+    let archivalLefteyeData = snapshot.val();
+
+    let keys;
+    let dataPoint;
+    keys = Object.keys(archivalLefteyeData);
+    for(let i = 0; i < keys.length; i++){
+      let key = keys[i];
+    dataPoint = archivalLefteyeData[key].imgChange;
+    arrLefteye.push(dataPoint);
+  }
+
+//get a random image data and send it out
+  let time = 5000 + Math.random() * 10000;
+  setInterval(() => {
+    randomIndex = Math.floor(Math.random() * arrLefteye.length);
+    randomLefteyeItem = arrLefteye[randomIndex];
+    // console.log(randomLefteyeItem); // prints a random value from the array
+    socket.emit('archivalLefteyeData', randomLefteyeItem);
+  }, time); 
+})
+
+
+
+let randomRighteyeItem;
+let countRighteye; //for refreshing the images from the database
+let countRighteyeCur; //for allocating part
+let arrRighteye = [];
+
+ref1.once('value').then((snapshot) => {
+  countRighteyeCur = snapshot.numChildren();
+  console.log("heyyyy i am always here", countRighteyeCur)
+  counter = counter + 1;
+})
+
+  // setInterval(()=>{
+  //       ref1.once('value').then((snapshot) => {
+  //        countRighteye = snapshot.numChildren();
+  //       console.log("righteye total images", countRighteye);
+  //     });
+  //  },10000)
+
+
+  ref1.once('value').then((snapshot) => {
+    let archivalRighteyeData = snapshot.val();
+
+
+    let keys;
+    let dataPoint;
+    keys = Object.keys(archivalRighteyeData);
+    for(let i = 0; i < keys.length; i++){
+      let key = keys[i];
+    dataPoint = archivalRighteyeData[key].imgChange;
+    arrRighteye.push(dataPoint);
+  }
+
+
+
+  let time = 5000 + Math.random() * 10000;
+  setInterval(() => {
+    randomIndex = Math.floor(Math.random() * arrRighteye.length);
+    randomRighteyeItem = arrRighteye[randomIndex];
+    // console.log(randomRighteyeItem); // prints a random value from the array
+    socket.emit('archivalRighteyeData', randomRighteyeItem);
+  }, time); 
+})
+
+
+
+
+
+
+
+
+let randomNoseItem; 
+let countNoseCur;
+let arrNose = [];
+
+ref2.once('value').then((snapshot) => {
+  countNoseCur = snapshot.numChildren();
+  console.log("heyyyy i am always here", countNoseCur)
+  counter = counter + 1;
+})
+
+  ref2.once('value').then((snapshot) => {
+    let archivalNoseData = snapshot.val();
+
+
+    let keys;
+    let dataPoint;
+    keys = Object.keys(archivalNoseData);
+    for(let i = 0; i < keys.length; i++){
+      let key = keys[i];
+    dataPoint = archivalNoseData[key].imgChange;
+    arrNose.push(dataPoint);
+  }
+
+
+
+  let time = 5000 + Math.random() * 10000;
+  setInterval(() => {
+    // console.log(arrNose.length);
+    randomIndex = Math.floor(Math.random() * arrNose.length);
+    randomNoseItem = arrNose[randomIndex];
+    // console.log(randomNoseItem); // prints a random value from the array
+    socket.emit('archivalNoseData', randomNoseItem);
+  }, time); 
+})
+
+
+
+
+let randomForeheadItem;
+let countForeheadCur; //for allocating part
+    let arrForehead = [];
+
+ref3.once('value').then((snapshot) => {
+  countForeheadCur = snapshot.numChildren();
+  console.log("heyyyy i am always here", countForeheadCur)
+  counter = counter + 1;
+})
+
+  // setInterval(()=>{
+  //       ref1.once('value').then((snapshot) => {
+  //       countForehead = snapshot.numChildren();
+  //       console.log("forehead total images", countForehead);
+  //     });
+  //  },10000)
+
+
+  ref3.once('value').then((snapshot) => {
+    let archivalForeheadData = snapshot.val();
+
+    let keys;
+    let dataPoint;
+    keys = Object.keys(archivalForeheadData);
+    for(let i = 0; i < keys.length; i++){
+      let key = keys[i];
+    dataPoint = archivalForeheadData[key].imgChange;
+    arrForehead.push(dataPoint);
+  }
+
+
+
+  let time = 5000 + Math.random() * 10000;
+  setInterval(() => {
+    // console.log("This is the forehead array", arrForehead.length);
+    randomIndex = Math.floor(Math.random() * arrForehead.length);
+    randomForeheadItem = arrForehead[randomIndex];
+    // console.log(randomForeheadItem); // prints a random value from the array
+    socket.emit('archivalForeheadData', randomForeheadItem);
+  }, time); 
+})
+
+
+
+
+
+
+let randomMouthItem;
+let countMouthCur; //for allocating part
+let arrMouth = [];
+
+ref4.once('value').then((snapshot) => {
+  countMouthCur = snapshot.numChildren();
+  console.log("heyyyy i am always here", countMouthCur)
+  counter = counter + 1;
+})
+
+  ref4.once('value').then((snapshot) => {
+    let archivalMouthData = snapshot.val();
+
+    let keys;
+    let dataPoint;
+    keys = Object.keys(archivalMouthData);
+    for(let i = 0; i < keys.length; i++){
+      let key = keys[i];
+    dataPoint = archivalMouthData[key].imgChange;
+    arrMouth.push(dataPoint);
+  }
+
+
+
+  let time = 5000 + Math.random() * 10000;
+  setInterval(() => {
+    randomIndex = Math.floor(Math.random() * arrMouth.length);
+    randomMouthItem = arrMouth[randomIndex];
+    // console.log(randomMouthItem); // prints a random value from the array
+    socket.emit('archivalMouthData', randomMouthItem);
+  }, time); 
+})
+
+
+
+
+
+
+
+
+let lowestValue;
+
+let load = setInterval(() => {
+  console.log("load");
+  if (counter == 5){
+    lowestValue = Math.min(countRighteyeCur, countLefteyeCur, countNoseCur, countForeheadCur, countMouthCur);
+    console.log("I am the lowest!!!!!", lowestValue); 
+
+
+
+      //   //SEND THE IMAGE DATA TO THE DATABASE
+      // if( socket.id == rooms[num].parts.lefteye.id){
+      //   rooms[num].parts.lefteye.imgData = newImg
+      //   ref0.push(newImg)
+      
+
+    if(lowestValue == countRighteyeCur){
+      console.log("righteye!")
+      let part = "righteye";
+      socket.emit('righteye', part);
+
+    }else if(lowestValue == countLefteyeCur){
+      console.log("lefteye!")
+      let part = "lefteye";
+      socket.emit('lefteye', part);
+
+    }else if(lowestValue == countNoseCur){
+      console.log("nose!")
+      let part = "nose";
+      socket.emit('nose', part);
+
+    }else if(lowestValue == countForeheadCur){
+      console.log("forehead!")
+      let part = "forehead";
+      socket.emit('forehead', part);
+
+    }else if(lowestValue == countMouthCur){
+      console.log("mouth!")
+      let part = "mouth";
+      socket.emit('mouth', part);
+    }
+
+    clearInterval(load);
+  }
+}, 500); 
+
+
+
+
+
+
+
+
+socket.on("imgLefteyeChange", newImg=>{
+    ref0.push(newImg)
+    // console.log(newImg)
+    arrLefteye.push(newImg.imgChange);
+})
+
+socket.on("imgRighteyeChange", newImg=>{
+   ref1.push(newImg)
+   arrRighteye.push(newImg.imgChange);
+})
+
+socket.on("imgNoseChange", newImg=>{
+   ref2.push(newImg);
+   arrNose.push(newImg.imgChange);
+})
+
+socket.on("imgForeheadChange", newImg=>{
+  ref3.push(newImg)
+  arrForehead.push(newImg.imgChange);
+})
+
+socket.on("imgMouthChange", newImg=>{
+  ref4.push(newImg)
+  arrMouth.push(newImg.imgChange);
+})
+
+
+
+
+
+
+    socket.on('disconnect', () => {
+        console.log('------------------------------------');
+        console.log('[-] user disconnected', socket.id);
+
+    });
+
+
+});
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+
+
+  
